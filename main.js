5b23a7cf1709594283bda4b6bbc41a76e1113a5b
Starystars67 <starystars67@gmail.com>
2020-01-10 00:24:29 +0000

    ITS ALIVE!?!?

--- main.js
+++ main.js
@@ -60,3 +60,156 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// Settings
+///////////////////////////////////////////////
+var checkboxes = {
+    ["TCPIN"]: false,
+    ["TCPOUT"]: false,
+    ["UDPIN"]: false,
+    ["UDPOUT"]: false,
+    ["WSIN"]: false,
+    ["WSOUT"]: false,
+}
+var Name = "Link"
+
+// IPC Handler
+///////////////////////////////////////////////
+ipcMain.on('hello', (event, args) => {
+  console.log("Hello Event: "+args)
+  event.sender.send('fromMain','Hi, asyn reply');
+});
+
+ipcMain.on('checkboxes', (event, args) => {
+  checkboxes = args;
+});
+
+ipcMain.on('control', (event, args) => {
+  //console.log(args)
+  if (args.option == "START") {
+    Start(args);
+  } else if (args.option == "RESET") {
+    Reset(args)
+  }
+});
+
+const net = require('net');
+var dgram = require('dgram');
+
+function Start(config) {
+  // TCP Server
+  ///////////////////////////////////////////////
+  var TCPserver = net.createServer();
+  TCPserver.listen(Number(config.local.tcp), () => {
+    win.webContents.send('console', `[TCP][${Name}] TCP Ready & Listening on port: ${config.local.tcp}`);
+  });
+
+  TCPserver.on('connection', function(sock) {
+    // Show user that game connected
+    win.webContents.send('console', `[TCP][Game -> ${Name}] Game Connected`);
+    // TCP Client
+    ///////////////////////////////////////////////
+    var TCPclient = new net.Socket();
+    TCPclient.connect(Number(config.remote.tcp), config.remote.ip, function() {
+      win.webContents.send('console', `[TCP][${Name} --> Server] TCP Connected`);
+    });
+
+    TCPclient.on('data', function(cdata) {
+      if (checkboxes.TCPIN) {
+        win.webContents.send('console', `[TCP][Server --> Client] ${cdata}`);
+      }
+      sock.write(cdata);
+    });
+
+    TCPclient.on('close', function() {
+      //console.log('Connection closed');
+    });
+
+
+    sock.on('data', function(data) {
+      if (checkboxes.TCPOUT) {
+        win.webContents.send('console', `[TCP][Client --> Server] ${data}`);
+      }
+      TCPclient.write(data);
+    });
+
+    sock.on('close', function(data) {
+      win.webContents.send('console', `[TCP][Game -> ${Name}] Game Disconnected`);
+    });
+
+    sock.on('error', (err) => {
+      win.webContents.send('console', `[TCP][${Name}] TCP Error Seek Developer Help!`);
+      win.webContents.send('err', err);
+      console.error(err);
+    });
+  });
+
+
+
+  // UDP Server
+  ///////////////////////////////////////////////
+  var UDPserver = dgram.createSocket('udp4');
+  var Rinfo = {}
+  UDPserver.on('listening', function() {
+    win.webContents.send('console', `[UDP][${Name}] UDP Ready & Listening on port: ${config.local.udp}`);
+  });
+  // UDP Client
+  ///////////////////////////////////////////////
+  var UDPclient = dgram.createSocket('udp4');
+  UDPclient.on('message',function(msg,info){
+    if (checkboxes.UDPIN) {
+      win.webContents.send('console', `[UDP][Server --> Client] ${msg.toString()}`);
+    }
+    UDPserver.send(msg,Rinfo.port,Rinfo.address,function(err){
+      if(err){
+        win.webContents.send('console', `[TCP][${Name}] UDP(server) Error Seek Developer Help!`);
+        win.webContents.send('err', err);
+        console.error(err);
+      }else{
+        //console.log('Data sent !!!');
+      }
+    });
+  });
+  UDPserver.on('message',function(msg,rinfo){
+    Rinfo = rinfo;
+    if (checkboxes.UDPOUT) {
+      win.webContents.send('console', `[UDP][Client --> Server] ${msg.toString()}`);
+    }
+    UDPclient.send(msg,Rinfo.port,Rinfo.address,function(err){
+      if(err){
+        win.webContents.send('console', `[TCP][${Name}] UDP (client) Error Seek Developer Help!`);
+        win.webContents.send('err', err);
+        console.error(err);
+      }else{
+        //console.log('Data sent !!!');
+      }
+    });
+  });
+  UDPserver.bind(Number(config.local.udp));
+
+  // WS Client
+  ///////////////////////////////////////////////
+
+  // WS Server
+  ///////////////////////////////////////////////
+  /*const WebSocket = require('ws');
+
+  const wss = new WebSocket.Server({ port: wsport });
+
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+      console.log('[WS] received: %s', message);
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(message);
+        }
+      });
+    });
+
+    ws.send('Welcome!');
+  });*/
+}
+
+function RESET() {
+
+}
--- script.js
+++ script.js
@@ -1,6 +1,89 @@
+// IPC Setup
+///////////////////////////////////////////////
+const ipc = require('electron').ipcRenderer;
+ipc.send('hello','UI Loaded');
+
+// Settings
+///////////////////////////////////////////////
+var checkboxes = {
+    ["TCPIN"]: false,
+    ["TCPOUT"]: false,
+    ["UDPIN"]: false,
+    ["UDPOUT"]: false,
+    ["WSIN"]: false,
+    ["WSOUT"]: false,
+}
+
+// Console Scrolling handler
+///////////////////////////////////////////////
 function updateScroll(){
     var element = document.getElementById("live-console");
     element.scrollTop = element.scrollHeight;
 }
 
 setInterval(updateScroll,10000);
+
+// Checkbox handler + Button
+///////////////////////////////////////////////
+
+$(document).ready(function(){
+    $('input[type="checkbox"]').click(function(){
+        if($(this).prop("checked") == true){
+            checkboxes[$(this).attr('id')] = true;
+            console.log($(this).attr('id')+" = true");
+        }
+        else if($(this).prop("checked") == false){
+            checkboxes[$(this).attr('id')] = false;
+            console.log($(this).attr('id')+" = false");
+        }
+        // Send updated choices to main
+        ipc.send('checkboxes',checkboxes);
+    });
+    $('button').click(function(){
+        if($(this).attr('id') == "START"){
+            //alert("START!")
+            var config = {
+                ["option"]: "START",
+                ["local"]: {
+                    ["tcp"]: $("#TCPport").val(),
+                    ["udp"]: $("#UDPport").val(),
+                    ["ws"]: $("#WSport").val(),
+                },
+                ["remote"]: {
+                    ["ip"]: $("#SERVERip").val(),
+                    ["tcp"]: $("#sTCPport").val(),
+                    ["udp"]: $("#sUDPport").val(),
+                    ["ws"]: $("#sWSport").val(),
+                },
+            };
+            ipc.send('control', config)
+        }
+        else if($(this).attr('id') == "RESET"){
+            //alert("RESET!")
+        }
+        // Send updated choices to main
+        ipc.send('checkboxes',checkboxes);
+    });
+});
+
+// IPC / Main to renderer handler + console
+///////////////////////////////////////////////
+
+ipc.on('fromMain', (event, messages) => {
+ console.log(messages)
+});
+
+ipc.on('console', (event, messages) => {
+ console.log(messages)
+ ConsoleLog(messages)
+ updateScroll()
+});
+
+ipc.on('err', (event, messages) => {
+ console.log(messages)
+ alert(messages)
+});
+
+function ConsoleLog(msg){
+    $("#live-console").append('<li>'+msg+'</li>');
+}
